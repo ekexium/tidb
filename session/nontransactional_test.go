adbd7bd8bd96377f5d2cae9952d847803e7f56c9
ekexium <ekexium@gmail.com>
2022-05-07 17:13:50 +0800

    add another self-referential case
    Signed-off-by: ekexium <ekexium@gmail.com>

--- session/nontransactional_test.go
+++ session/nontransactional_test.go
@@ -484,3 +484,57 @@ func TestBug(t *testing.T) {
 	tk2.MustExec(delete1)
 	tk2.MustQuery("select count(*) from tbl_3").Check(testkit.Rows("3"))
 }
+
+func TestAnother(t *testing.T) {
+	store, clean := createStorage(t)
+	defer clean()
+	tk1 := testkit.NewTestKit(t, store)
+	tk2 := testkit.NewTestKit(t, store)
+	tk1.MustExec("use test")
+	tk2.MustExec("create database test2")
+	tk2.MustExec("use test2")
+	tk1.MustQuery("SELECT VARIABLE_VALUE FROM mysql.tidb WHERE VARIABLE_NAME='new_collation_enabled';").Check(testkit.Rows("True"))
+	initSqls := []string{
+		"create table tbl_6 ( col_26 int  unsigned  default 2056354103 ,col_27 datetime    default '2007-11-22' ,col_28 set ( 'Alice','Bob','Charlie','David' )    default 'David' ,col_29 double   not null ,col_30 varbinary ( 101 ) , primary key  ( col_26 ,col_30 ( 5 ) ) /*T![clustered_index] clustered */ ,unique key idx_12 ( col_26 ,col_29 ,col_30 ) ) charset binary collate binary ;",
+	}
+	sqls := []string{
+		"insert  into tbl_6 set col_26 = 1327391217, col_27 = '1981-07-29', col_28 = 'Bob', col_29 = 6940.277413153138, col_30 = 'st%q' ;",
+		"insert  into tbl_6 set col_26 = 3339870142, col_27 = '1987-03-03', col_28 = null, col_29 = 2700.3218418960823, col_30 = '(F6D$XoHIyYtb!z' on duplicate key update col_29 = 5063.589929518924, col_30 = 'g', col_27 = '1987-01-24', col_26 = 3213514720, col_28 = 'Charlie';",
+		"insert ignore into tbl_6 set col_26 = 3887764760, col_27 = '1972-04-08', col_28 = 'David', col_29 = 1588.3246603648506, col_30 = 'ZRrsa~' on duplicate key update col_28 = 'David', col_29 = 7040.541372843526;",
+		"split on col_27 limit 3 delete from tbl_6 where not( tbl_6.col_28 between 'Bob' and 'David' );",
+		"replace into tbl_6 set col_26 = 3564242001, col_27 = '1971-10-04', col_28 = 'David', col_29 = 1560.2503971653398, col_30 = '';",
+		"insert ignore into tbl_6 (col_26,col_27,col_28,col_29,col_30) values ( 2803625120,'1993-02-27','Alice',1415.5466240555938,'UR7q1K_7SRuYjnj' ) ,( 2102741138,'1987-01-19','David',1766.1523453869688,'yer+V' ) ,( 4094568466,'2017-06-08','Bob',7507.627086484442,'oRMZ~D$e(j' ) ,( 802897932,'2011-01-14','Charlie',1315.0805833905963,'VTzST$ngPg7sO9B' ) ,( 3572282198,'2013-03-14','David',6068.083318161412,'-)' ) ,( 3734763546,'2034-09-11','Charlie',8919.968095050659,'#p2yu=AJW' ) ,( 626433334,'1975-09-15','Charlie',7762.893290897909,'CGWv~WAY9gmZU7Z)' ) on duplicate key update col_30 = 'ig^';",
+		"replace into tbl_6 (col_26,col_27,col_28,col_29,col_30) values ( 3425555681,'1971-03-12','Charlie',6365.263720999394,'()6*YNeX' ) ,( 3792616342,'2010-06-28','Alice',4198.852411621481,'N+D8' ) ,( 3875789222,'1985-10-08','Alice',5261.721461179885,'Znt20k&O=6=BP^' ) ,( 4022660468,'2036-08-08',null,3071.424436796261,'HH_8(j1RXz' ) ,( 2094128545,'1990-10-11','Bob',9673.52859595257,'q' );",
+		"set @@global.tidb_enable_clustered_index = 1;",
+		"replace into tbl_6 set col_26 = 1925477354, col_27 = '2031-07-31', col_28 = 'David', col_29 = 5171.338668077971, col_30 = 'RIhWRo';",
+		"replace into tbl_6 (col_26,col_27,col_28,col_29,col_30) values ( 271972963,'2017-12-23','Alice',6178.777742637576,'8f+Q-P' ) ,( 2692025906,'2014-07-03',null,6917.9055799517155,'' );",
+		"insert  into tbl_6  values ( 1372700923,'1988-02-18','Alice',8439.096674397673,'9g_-g1gGv3nU36' ) ,( 3611613860,'2025-12-23','Bob',9556.273521447156,'lz5-' ) on duplicate key update col_26 = 904362433, col_30 = 'L_yw-_JZv-K', col_29 = 644.7638948370844, col_27 = '1986-02-19', col_28 = 'David';",
+		"split on col_30 limit 2 delete from tbl_6 where tbl_6.col_26 > 1242534189 or not( tbl_6.col_28 in ( select col_28 from tbl_6 where IsNull( tbl_6.col_28 ) ) );",
+	}
+
+	// query := "SELECT * FROM tbl_3 ORDER BY col_11, col_12, col_13, col_14, col_15;"
+	query := "SELECT count(*) FROM tbl_6"
+
+	for _, sql := range initSqls {
+		tk1.MustExec(sql)
+		tk2.MustExec(sql)
+	}
+	for _, sql := range sqls {
+		tk1.Exec(sql)
+		if strings.HasPrefix(sql, "split") {
+			sql = sql[strings.Index(sql, "delete"):]
+		}
+		tk2.Exec(sql)
+		println(sql)
+		tk1.MustQuery(query).Check(tk2.MustQuery(query).Rows())
+	}
+	rows := tk1.MustQuery("SELECT * FROM tbl_6").Rows()
+	for _, row := range rows {
+		for _, col := range row {
+			print(col.(string), " ")
+		}
+		println()
+	}
+	// tk1.MustQuery("select count(*) from tbl_3").Check(tk2.MustQuery("select count(*) from tbl_3").Rows())
+	// println(tk1.MustQuery("select count(*) from tbl_3").Rows()[0][0].(string))
+}
